0094eb07b6975a3ff997790e8b73fa0f4c5f79be
Arslan <arslanabakarov79@gmail.com>
2025-09-16 20:23:38 +0400

    Создание проекта

---
+++ KopLab21/KopLab21/ListOfValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KopLab21
+{
+    public partial class ListOfValue : UserControl
+    {
+        public ListOfValue()
+        {
+            InitializeComponent();
+        }
+    }
+}
